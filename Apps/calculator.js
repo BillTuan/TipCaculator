34b4e232b72fda1e09e0f83e0dea4c082312157f
DESKTOP-7BEQCBT\Bill <baotuan0.0@gmail.com>
2017-05-21 13:38:47 +0700

    add setting page

--- Apps/calculator.js
+++ Apps/calculator.js
@@ -1,11 +1,32 @@
 /* @flow */
 
 import React, {Component} from 'react';
-import {AppRegistry, StyleSheet, Text, View, TextInput} from 'react-native';
-
+import {AppRegistry, StyleSheet, Text, View, TextInput, Button, Picker, AsyncStorage} from 'react-native';
+import{Navigator} from "react-native-deprecated-custom-components";
 import SegmentedControlTab from 'react-native-segmented-control-tab';
 
 export default class Cal extends Component {
+  renderScene(route, navigator){
+    switch (route.name) {
+      case "main": return(<CalculatorPage toSetting = {() => {navigator.push({name:"setting"})}}/>);
+      case "setting": return(<Setting backMain = {() => {navigator.pop({name:"main"})}}/>);
+    }
+  }
+  configureScene(route, routeStack){
+    return Navigator.SceneConfigs.FloatFromRight;
+  }
+  render() {
+    return (
+      <Navigator
+        initialRoute={{name:"main"}}
+        renderScene={this.renderScene}
+        configureScene={ this.configureScene }
+      />
+    );
+  }
+}
+
+class CalculatorPage extends Component{
   constructor() {
     super()
     this.state = {
@@ -52,11 +73,14 @@ export default class Cal extends Component {
     return ['10%', '15%', '50%'];
   }
 
-
-
   render() {
     return (
       <View>
+        <Button
+             tyle={{flex:1, margin:10, fontSize:20}}
+             title="Setting"
+             onPress={this.props.toSetting}
+           />
         <View>
           <Text style={styles.title}>Tip Calculator</Text>
         </View>
@@ -97,6 +121,84 @@ export default class Cal extends Component {
   }
 }
 
+class Setting extends Component{
+
+  constructor(){
+    super()
+    this.state = {
+      sceneTransition: "FloatFromRight",
+      scene: "",
+    }
+  }
+
+  setSelectSceneTransition(scene){
+    try {
+      this.setSceneTransition(scene);
+      this.setState({
+        scene: scene
+      });
+    } catch (error) {
+      console.log("Oop!! Something went wrong !!!" + error);
+    }
+  }
+
+  async setSceneTransition(scene){
+    try{
+      await AsyncStorage.setItem('SCENE_SELECTED', scene);
+      this.setState({
+        sceneTransition : scene
+      })
+    }catch(error){
+       console.log("Hmm, something when wrong when set data..." + error);
+    }
+  }
+
+  componentDidMount(){
+    this.getSceneTransition();
+  }
+
+  async getSceneTransition(){
+  try{
+    let sceneTransitionValue = await AsyncStorage.getItem("SCENE_SELECTED");
+    // Store value to State
+    this.setState({
+      sceneTransition : sceneTransitionValue
+    });
+  }catch(error){
+    console.log("Hmm, something when wrong when get data..." + error);
+  }
+}
+
+  render(){
+    return(
+      <View>
+        <Button
+          style={{width:10, flex:0.1}}
+          title="Go Back"
+          onPress={this.props.backMain}
+        />
+
+        <View style={{marginTop:50,padding:10}}>
+        <View>
+          <Text style={{fontSize:25}}>Scene Transitions</Text>
+          <Picker
+            selectedValue={this.state.sceneTransition}
+            onValueChange={(scene) => this.setSelectSceneTransition(scene)}>
+            <Picker.Item label="FloatFromRight" value="FloatFromRight" />
+            <Picker.Item label="FloatFromLeft" value="FloatFromLeft" />
+            <Picker.Item label="FloatFromBottom" value="FloatFromBottom" />
+            <Picker.Item label="FloatFromBottomAndroid" value="FloatFromBottomAndroid" />
+            <Picker.Item label="SwipeFromLeft" value="SwipeFromLeft" />
+            <Picker.Item label="HorizontalSwipeJump" value="HorizontalSwipeJump" />
+            <Picker.Item label="HorizontalSwipeJumpFromRight" value="HorizontalSwipeJumpFromRight" />
+          </Picker>
+        </View>
+      </View>
+
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
   container: {
       flex: 1,
